ee778b8a387c6cb3b1825cc92a9194ad36cf0175
aparna g <aparna.ganesh1542@gmail.com>
2015-08-27 14:51:21 -0700

    CODE FIX - DISPLAY PRODUCTS AFTER ADMIN UPDATE

--- DPProjectSmartCals_NEW/src/edu/scu/dp/smartcals/admin/AdminOperations.java
+++ DPProjectSmartCals_NEW/src/edu/scu/dp/smartcals/admin/AdminOperations.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import edu.scu.dp.smartcals.exception.AdminOperationsException;
+import edu.scu.dp.smartcals.model.InventoryModel;
 import edu.scu.dp.smartcals.model.NutritionalInfoModel;
 import edu.scu.dp.smartcals.model.ProductModel;
 import edu.scu.dp.smartcals.vm.Product;
@@ -19,19 +20,75 @@ import edu.scu.dp.smartcals.vm.VendingMachine;
  * Interface to define Admin operations
  */
 public interface AdminOperations {
+	
+	/**
+	 * API to search product inventory for a given Product ID
+	 * @param prodId
+	 * @return
+	 */
+	public InventoryModel searchInventory(long prodId);
 
-	public void reStockProduct(long vmId,long productId,int quantity);
+
+	/**
+	 * API to modify product inventory 
+	 * @param productId
+	 * @param price
+	 * @param vmId
+	 * @param quantity
+	 * @return
+	 */
+	public boolean modifyInventory(long productId,double price, int vmId, int quantity);
+	
+	/**
+	 * API to add new product to the inventory
+	 * @param productId
+	 * @param price
+	 * @param vmId
+	 * @param quantity
+	 * @return
+	 */
+	public boolean addInventoryData(int productId,double price,int vmId,int quantity);
+	
+	/**
+	 * API to delete product data from the inventory
+	 * @param prodId
+	 * @param vmId 
+	 * @return
+	 */
+	public boolean deleteInventory(long prodId, long vmId);
 	
 	//code change-Aparna 08/24
 	
+	/**
+	 * API to get Product data
+	 * @param productId
+	 * @return
+	 * @throws AdminOperationsException
+	 */
 	public ProductModel getProduct(long productId) throws AdminOperationsException;
 	
 	
 	//code change-Aparna 08/24
 	
+	/**
+	 * API to register for alert notifications
+	 * @param alertListener
+	 */
 	 public void addAlertListeners(AlertListener alertListener);
 	 
+	 /**
+	  * API to register for inventory update notifications
+	  * @param invUpdateListener
+	  */
+	 public void addInventoryUpdateListeners(InventoryUpdateListener invUpdateListener);
+	 
 	 //code change-Aparna 08/25
+	 /**
+	  * API to update Product info( Price,category,Price)	
+	  * @param product
+	  * @param productId
+	  * @throws SQLException
+	  */
 	 public void updateProduct(Product product,long productId) throws SQLException;
 	//---------------------------------------------------
 	/**
@@ -56,6 +113,9 @@ public interface AdminOperations {
 	public boolean updateNewNutriInfo(ArrayList<String> dataValues) throws SQLException;
 	
 	public boolean deleteNutriInfo(long productID) throws SQLException;
+
+	
+	
 }
 
 
